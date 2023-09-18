0d4f6c7661a11ba5a660f8ce448d57c4d69dc43f
lidefi87 <lidefi87@hotmail.com>
2023-09-18 17:35:57 +1000

    Providing more details about this function

--- R_based_scripts/Installing_R_libraries.R
+++ R_based_scripts/Installing_R_libraries.R
@@ -1,4 +1,10 @@
-#Function checking that all packages used in repository are installed
+# Installing R libraries to run rimrep-examples repository
+#
+# `checking_libraries` function checks that all R libraries used in the 
+# rimrep-examples repository are in fact installed in your local machine. If 
+# any libraries are missing, it will install them automatically.
+# 
+
 checking_libraries <- function(){
   #List of packages needed to run all notebooks in repository
   packages_required <- c("arrow", "tidyverse", "wkb", "sf", "rnaturalearth", 
@@ -13,6 +19,7 @@ checking_libraries <- function(){
   
   #If packages are missing, install them
   if(length(packages_needed)){
+    print(paste0("The following packages will be installed: ", paste(packages_needed, collapse = ", ")))
     install.packages(packages_needed)
     #If no packages are missing, print message
     }else{print("All packages needed to run notebooks in this repository are available in your machine.")}
