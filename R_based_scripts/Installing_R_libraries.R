b118a427a790857f3cacbdfa0cee80de42833b78
lidefi87 <lidefi87@hotmail.com>
2023-09-18 16:52:01 +1000

    adding new libraries used in data cleaning process for SELTMP data

--- R_based_scripts/Installing_R_libraries.R
+++ R_based_scripts/Installing_R_libraries.R
@@ -1,7 +1,9 @@
 #Function checking that all packages used in repository are installed
 checking_libraries <- function(){
   #List of packages needed to run all notebooks in repository
-  packages_required <- c("arrow", "tidyverse", "wkb", "sf", "rnaturalearth", "leaflet", "tictoc", "DT", "wordcloud2", "janitor")
+  packages_required <- c("arrow", "tidyverse", "wkb", "sf", "rnaturalearth", 
+                         "leaflet", "tictoc", "DT", "wordcloud2", "janitor",
+                         "magrittr", "tm")
   
   #Checking packages installed in local machine
   packages_local <- installed.packages()
