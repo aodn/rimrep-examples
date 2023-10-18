025410f5c6ee9ed10dc0ce518ab6059ce3cebd73
lidefi87 <lidefi87@hotmail.com>
2023-10-18 17:36:58 +1100

    remove script showing use of connect_dms_dataset function. Replaced by markdown

--- R_based_scripts/accessing_netcdfs.R
+++
@@ -1,8 +0,0 @@
-library(terra)
-source("R_based_scripts/useful_functions.R")
-
-base_url <-"https://pygeoapi.staging.reefdata.io/collections/noaa-crw-dhw/"
-
-ras <- connect_dms_dataset(base_url, start_time = "2023-02-01")
-
-plot(ras)
